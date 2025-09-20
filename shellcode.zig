0f22fa88d2087231fe4055af09e1c2b8bacc4094
serr <sc7227484@gmail.com>
2025-09-20 17:52:09 +0300

    quick update

--- shellcode.zig
+++ shellcode.zig
@@ -1,47 +1,37 @@
-inline fn exit(status: usize) noreturn {
-    _ = asm volatile (
-        \\mov $60, %%rax
-        \\mov %[status], %%rdi
-        \\syscall
-        :
-        : [status] "{rdi}" (status)
-        : .{ .rax = true, .rcx = true, .r11 = true }
-    );
+
+inline fn
+syscall1(number: usize, arg1: usize) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+        : .{ .rcx = true, .r11 = true });
+}
+
+inline fn
+syscall3(number: usize, arg1: usize, arg2: usize, arg3: usize) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+          [arg2] "{rsi}" (arg2),
+          [arg3] "{rdx}" (arg3),
+        : .{ .rcx = true, .r11 = true });
+}
+
+inline fn
+exit(status: usize) noreturn {
+    _ = syscall1(60, status);
     unreachable;
 }
 
-// assemvly syntax in zig
-
-// asm [volatile] (
-//     "assembly template"
-//     :
-//     outputs
-//     :
-//     inputs
-//     :
-//     clobbers
-// )
-
-inline fn write(fd: usize, buf: [*]u8, count: usize) usize {
-    return asm volatile (
-        \\mov $1, %%rax
-        \\mov %[fd], %%rdi
-        \\mov %[buf], %%rsi
-        \\mov %[count], %%rdx
-        \\syscall
-        :
-        [ret] "={rax}" (-> usize) // return value syntax
-        :
-        [fd] "{rdi}" (fd),
-        [buf] "{rsi}" (buf),
-        [count] "{rdx}" (count)
-        :
-        // affected registers
-        .{ .rax = true, .rcx = true, .r11 = true }
-    );
+inline fn
+write(fd: usize, buf: [*]u8, count: usize) usize {
+    return syscall3(1, fd, @intFromPtr(buf), count);
 }
 
-export fn _start() noreturn {
+export fn
+_start() noreturn {
     var msg = [_]u8{
         'h', 'e', 'l', 'l', 'o', ' ',
         's', 'a', 'i', 'l', 'o', 'r', '!',
